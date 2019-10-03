145946e193a68686ce794e15d351d10548e169e2
Jason Yu <jyu031103@gmail.com>
2019-10-02 20:19:05 -0700

    Update part1.js

--- Unit 1 - JavaScript (Doodle Jump)/part1.js
+++ Unit 1 - JavaScript (Doodle Jump)/part1.js
@@ -1,23 +1,17 @@
-
-//variables
-{
+// Define variables for Player Control
 let playerX = 180;
 let playerY = 400;
 let playerYVel = -12;
-let playerYAcc = 0.5;
-}
+const playerYAcc = 0.5;
 
-//setup
-function setup() {
+setup = () => {
   createCanvas(400, 600);
 }
 
-function draw() {
-  //player graphics and updating
-  {
+draw = () => {
+  // player graphics and movement
   background(255);
   rect(playerX, playerY, 20, 20, 5);
   playerY = playerY + playerYVel;
   playerYVel = playerYVel + playerYAcc;
-  } 
 }
